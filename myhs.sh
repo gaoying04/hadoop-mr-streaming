86b2a2c6b49638366fdeab0c8c5eb705f298c812
Miles Porter <mporter@paintedharmony.com>
2017-10-31 09:29:31 -0500

    Trying again.

--- myhs.sh
+++ myhs.sh
@@ -4,4 +4,5 @@ echo $1
 echo $2
 echo $3
 echo $4
-hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -files apache_parser,$1, $2 -mapper "$1" -reducer "$2" -input $3 -output $4
+hadoop fs -rm -r -f $4
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -files apache_parser.py,$1,$2 -mapper "$1" -reducer "$2" -input $3 -output $4
