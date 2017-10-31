01d453cbae9b7c21bbd80549f6ae110ede40a24e
Miles Porter <mporter@paintedharmony.com>
2017-10-31 08:54:45 -0500

    Trying to add parser back in.

--- apache_log_mapper.py
+++ apache_log_mapper.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 import sys
-# from apache_parser import apache_parser as Parser
+from apache_parser import apache_parser as Parser
 import logging
 # logging.basicConfig(filename='mapper.log', level=logging.DEBUG)
 
 # sysin = sys.stdin
 # sysout = sys.stdout
-# parser = Parser()
+parser = Parser()
 
 
 # def save_data(request, host):
@@ -17,10 +17,9 @@ import logging
 #    logging.debug("Starting mapper job")
 try:
     for line in sys.stdin:
-    #            data = parser.parse(line)
-        data = line.split()
+        data = parser.parse(line)
         if data is not None:
-            print("{0}\t{1}".format(data[0], data[6]))
+            print("{0}\t{1}".format(data["request"], data["time"]))
 except Exception as ex:
     logging.error("An error has occurred:\n{0}\n".format(ex.message))
 finally:
