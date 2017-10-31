2e9f07fa247a084acdde08b8fed77fa7bcac5f00
Miles Porter <mporter@paintedharmony.com>
2017-10-31 08:44:01 -0500

    Simplifying 2.

--- apache_log_mapper.py
+++ apache_log_mapper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 # from apache_parser import apache_parser as Parser
-# import logging
+import logging
 # logging.basicConfig(filename='mapper.log', level=logging.DEBUG)
 
 # sysin = sys.stdin
@@ -15,16 +15,16 @@ import sys
 
 #def parse():
 #    logging.debug("Starting mapper job")
-#    try:
-for line in sys.stdin:
-#            data = parser.parse(line)
-    data = line.split()
-    if data is not None:
-        print("{0}\t{1}".format(data[0], data[6]))
-# except Exception as ex:
-#     logging.error("An error has occurred:\n{0}\n".format(ex.message))
-# finally:
-#     logging.debug("Mapping complete. Closing local mapper log file.")
+try:
+    for line in sys.stdin:
+    #            data = parser.parse(line)
+        data = line.split()
+        if data is not None:
+            print("{0}\t{1}".format(data[0], data[6]))
+except Exception as ex:
+    logging.error("An error has occurred:\n{0}\n".format(ex.message))
+finally:
+    logging.debug("Mapping complete. Closing local mapper log file.")
 
 #Do the work
 # parse()
