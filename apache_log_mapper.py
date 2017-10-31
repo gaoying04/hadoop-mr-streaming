89c1070995d75d1f47b91348c3ec41a58ee920a6
Miles Porter <mporter@paintedharmony.com>
2017-10-31 08:41:30 -0500

    Simplifying.

--- apache_log_mapper.py
+++ apache_log_mapper.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python
 import sys
-from apache_parser import apache_parser as Parser
-import logging
-logging.basicConfig(filename='mapper.log', level=logging.DEBUG)
+# from apache_parser import apache_parser as Parser
+# import logging
+# logging.basicConfig(filename='mapper.log', level=logging.DEBUG)
 
-sysin = sys.stdin
-sysout = sys.stdout
-parser = Parser()
-
-
-def save_data(request, host):
-    print("{0}\t{1}".format(request.split()[1], host))
+# sysin = sys.stdin
+# sysout = sys.stdout
+# parser = Parser()
 
 
-def parse():
-    logging.debug("Starting mapper job")
-    try:
-        for line in sys.stdin:
-            data = parser.parse(line)
-            if data is not None:
-                save_data(data["request"], data["time"])
-    except Exception as ex:
-        logging.error("An error has occurred:\n{0}\n".format(ex.message))
-    finally:
-        logging.debug("Mapping complete. Closing local mapper log file.")
+# def save_data(request, host):
+#    print("{0}\t{1}".format(request.split()[1], host))
+
+
+#def parse():
+#    logging.debug("Starting mapper job")
+#    try:
+for line in sys.stdin:
+#            data = parser.parse(line)
+    data = line.split()
+    if data is not None:
+        print("{0}\t{1}".format(data[0], data[6]))
+# except Exception as ex:
+#     logging.error("An error has occurred:\n{0}\n".format(ex.message))
+# finally:
+#     logging.debug("Mapping complete. Closing local mapper log file.")
 
 #Do the work
-parse()
+# parse()
