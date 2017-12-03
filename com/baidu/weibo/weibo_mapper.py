556655871bc7ca93db324ad8b266ad417a50cf9a
gaoying04 <gaoying04@baidu.com>
2017-12-04 02:07:21 +0800

    mapper

--- com/baidu/weibo/weibo_mapper.py
+++ com/baidu/weibo/weibo_mapper.py
@@ -8,22 +8,21 @@ DATA_DIR = os.path.join(os.path.expanduser("~"), "data")
 print(DATA_DIR)
 #DATA_URL = "/".join(["http://cp01-yuanfang-01.epc.baidu.com:8080", "data"])
 def get_config_data():
-    fp = open(DATA_DIR,"wr")
+    fp = open(DATA_DIR,"w")
     line_list= []
     for line in fp.readline():
-        line_list.append(line)
+        line_list.append(line.strip())
     return line_list
 if __name__ == '__main__':
     for line  in sysin:
-        if line is not None and len(line) >3:
-            try:
-                line_hdfs_json = json.loads(line,encoding='utf-8')
+        hdfs_log = line.decode("uft-8")
+        try:
                 for word in get_config_data():
-                    if(word in str(line_hdfs_json)):
+                    if(word in str(hdfs_log)):
                         #print '%s%s%d' % (word, '\t', 1)
                         sys.stdout('%s%s%d' % (word, '\t', 1))
-            except Exception as e:
-                continue
+        except Exception as e:
+            continue
 
 
 
