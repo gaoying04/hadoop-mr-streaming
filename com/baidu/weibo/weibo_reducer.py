93b07197a748c022eb99823bf0c1183537d9e51a
gaoying04 <gaoying04@baidu.com>
2017-12-04 01:37:35 +0800

    reduce

--- com/baidu/weibo/weibo_reducer.py
+++ com/baidu/weibo/weibo_reducer.py
@@ -7,5 +7,8 @@ if __name__ == '__main__':
         world,num = line.strip().split('\t')
         count =  int(num)
         world_dic[world]=world_dic.get(world,0) +count
+        sys.stdout.write(str(world_dic))
+        sys.stdout.flush()
+
     print(str(world_dic))
 
