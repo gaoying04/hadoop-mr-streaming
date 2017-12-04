d9e8425f5ec0eaa421a8b83cf976a63913572f53
gaoying04 <gaoying04@baidu.com>
2017-12-04 14:21:01 +0800

    modify mapper

--- com/baidu/weibo/weibo_mapper.py
+++ com/baidu/weibo/weibo_mapper.py
@@ -2,19 +2,20 @@
 import os
 import sys
 import json
+
 sysin = sys.stdin
 sysout = sys.stdout
 DATA_DIR = os.path.join(os.path.expanduser("~"), "data")
 print(DATA_DIR)
 #DATA_URL = "/".join(["http://cp01-yuanfang-01.epc.baidu.com:8080", "data"])
 def get_config_data():
-    fp = open(DATA_DIR,"w")
+   fp=open(DATA_DIR,"w")
     line_list= []
     for line in fp.readline():
         line_list.append(line.strip())
     return line_list
 if __name__ == '__main__':
-    for line  in sysin:
+    for line  in sys.stdin:
         hdfs_log = line.decode("uft-8")
         try:
                 for word in get_config_data():
